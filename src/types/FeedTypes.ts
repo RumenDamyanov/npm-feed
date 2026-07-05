88cf3307f4e2f425d56631051949efc671a3f1ce
Rumen Damyanov <dev@rumenx.com>
2026-07-05 18:26:21 +0300

    fix: format errors

--- src/types/FeedTypes.ts
+++ src/types/FeedTypes.ts
@@ -71,4 +71,10 @@ export type FeedFormat = 'rss' | 'atom';
  * Change frequency for feed items
  */
 export type ChangeFrequency =
-  'always' | 'hourly' | 'daily' | 'weekly' | 'monthly' | 'yearly' | 'never';
+  | 'always'
+  | 'hourly'
+  | 'daily'
+  | 'weekly'
+  | 'monthly'
+  | 'yearly'
+  | 'never';
